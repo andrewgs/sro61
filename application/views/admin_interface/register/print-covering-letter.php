276627a0d85c10e0a18aa2ceef3dcac35145a5a1
Andrey Samoylov <andrew.samoilov@gmail.com>
2012-12-07 12:41:59 +0400

    documents modified

--- application/views/admin_interface/register/print-covering-letter.php
+++ application/views/admin_interface/register/print-covering-letter.php
@@ -3,16 +3,19 @@
 <?php $this->load->view('admin_interface/includes/head');?>
 <body>
 	<style type="text/css">
-		@media print {body, p { font-family: Tahoma, sans-serif; font-size: 16px; line-height: 24px; margin-bottom: 14px;}}
+		@media print { 
+			body, p { font-family: Cambria, 'Times New Roman', Times, serif; font-size: 16px; line-height: 24px; margin-bottom: 14px;}
+			body { padding: 60px; } 
+		}
 	</style>
 	<div class="container-fluid" style="position: relative; ">
 		<div class="row">
 			<div class="span12">
-				<div class="span6">
-					<p class="center title">
+				<div class="span5">
+					<p class="center title colored">
 						<img src="<?=$baseurl;?>images/print-logo.png" alt="" width="140"/><br/>
+						<span>Саморегулируемая организация</span><br/>
 						<span>Некоммерческое партнерство</span><br/>
-						<span>САМОРЕГУЛИРУЕМАЯ ОРГАНИЗАЦИЯ</span><br/>
 						<span>344002, г.Ростов-на-Дону, ул., Московская д.63, оф. 247</span><br/>
 						<span>Телефон: (863) 296-20-92, Факс: (863) 219-15-68</span><br/>
 						<span>E-mail: <?=safe_mailto('sro61@mail.ru','sro61@mail.ru');?> <?=anchor('http://sro61.ru/','http: sro61.ru');?></span><br/>
@@ -21,46 +24,53 @@
 						
 					</p>
 				</div>
-				<div class="span5">
+				<div class="span5 offset1">
 					<p class="center title">
 						<br/><br/><br/>
-						<span>Директору департамента</span><br/>
-						<span>энергоэффективности</span><br/>
-						<span>Модернизации и развития ТЭК</span><br/>
-						<span>Телефон: (863) 296-20-92, Факс: (863) 219-15-68</span><br/>
-						<span>Свистунову П.В.</span>
+						<strong>Директору департамента</strong><br/>
+						<strong>энергоэффективности</strong><br/>
+						<strong>модернизации и развития ТЭК</strong><br/>
+						<strong>Свистунову П.В.</strong>
 						
 					</p>
 				</div>
 				<div class="clear"></div>
-				<br/><br/><br/>
+			</div>
+	</div>
+	<div class="row">
+		<div class="span11">
+				<p>&nbsp;</p><p>&nbsp;</p>
 				<p class="center title">
-					<b>Уважаемый Павел Валентинович!</b>
+					<strong>Уважаемый Павел Валентинович!</strong>
 				</p>
 				<p class="intend">
 					Саморегулируемая организация Некоммерческое Партнерство «Энергоаудит» 
-					(регистрационный номер СРО-Э-101 от 07 июля 2011 года) просит Вас принять для регистрации энергетический паспорт 
-					№ <?=$passport['number'];?> составленный на: <?=$passport['customer'];?><br/>
+					(регистрационный номер <nobr>СРО-Э-101</nobr> от <nobr>07 июля 2011 года</nobr>) просит Вас принять для регистрации энергетический паспорт 
+					<nobr>№<?=$passport['number'];?></nobr> составленный на <?=$passport['customer'];?><br/>
 Юридический адрес:<br/> <?=$passport['address'];?><br/>
 ИНН <?=$passport['inn'];?><br/>
-членом СРО НП «Энергоаудит» - <?=$passport['member'];?>.<br/>
+членом СРО НП «Энергоаудит» - организацией <?=$passport['member'];?>.</p>
+				<p class="intend">
 	Основанием для проведения обязательного энергетического обследования является:<br/>
-	- Федеральный Закон « 261<br/>
-	- Постановление Правительства Российской Федерации от 25 января 2011 года № 19<br/>
-	- Приказа Минэнерго России от 19 апреля 2010 года № 182<br/><br/>
+	<ul class="nolist">
+	<li>- Федеральный Закон №261;</li>
+	<li>- Постановление Правительства Российской Федерации от 25 января 2011 года №19;</li>
+	<li>- Приказ Минэнерго России от 19 апреля 2010 года № 182.</li>
+	</ul></p>
 
-Приложения<br/>
--	Энергетический паспорт объекта на бумажном носителе.<br/>
--	CD диск, содержащий электронные копии энергетических паспортов в формате PDF и XML
+<p>Приложения:</p>
+<p class="intend">
+<ul class="nolist">
+<li>- Энергетический паспорт объекта на бумажном носителе;</li>
+<li>- CD диск, содержащий электронные копии энергетических паспортов в формате PDF и XML.</li>
 				</p>
+				<p>&nbsp;</p>
 				<p>
 					<table class="table no-border">
 						<tbody>
 							<tr>
-								<td width="75%">С уважением,<br/>Директор СРО НП «Энергоаудит»</td>
-								<td width="25%">Л.К. Евкина</td>
-								<td>&nbsp;</td>
-								<td>&nbsp;</td>
+								<td width="85%">С уважением,<br/>Директор СРО НП «Энергоаудит»</td>
+								<td width="15%"><em>Л.К. Евкина</em></td>
 							</tr>
 						</tbody>
 					</table>
@@ -68,5 +78,10 @@
 			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+		window.onload = function(){
+			//setTimeout(function() { window.print(); }, 1000);
+		};
+	</script>
 </body>
 </html>
--- application/views/admin_interface/register/print-sample-notice.php
+++ application/views/admin_interface/register/print-sample-notice.php
@@ -3,51 +3,57 @@
 <?php $this->load->view('admin_interface/includes/head');?>
 <body>
 	<style type="text/css">
-		@media print {body, p { font-family: Tahoma, sans-serif; font-size: 16px; line-height: 24px; margin-bottom: 14px;}}
+		@media print { 
+			body, p { font-family: Cambria, 'Times New Roman', Times, serif; font-size: 16px; line-height: 24px; margin-bottom: 14px;}
+			body { padding: 60px; } 
+		}
 	</style>
 	<div class="container-fluid" style="position: relative; ">
 		<div class="row">
 			<div class="span12">
-				<div class="span6">
-					<p class="center title">
+				<div class="span5">
+					<p class="center title colored">
 						<img src="<?=$baseurl;?>images/print-logo.png" alt="" width="140"/><br/>
+						<span>Саморегулируемая организация</span><br/>
 						<span>Некоммерческое партнерство</span><br/>
-						<span>САМОРЕГУЛИРУЕМАЯ ОРГАНИЗАЦИЯ</span><br/>
 						<span>344002, г.Ростов-на-Дону, ул., Московская д.63, оф. 247</span><br/>
-						<span>Телефон: (863) 296-20-92, Факс: (863) 219-15-68</span><br/>
-						<span>E-mail: <?=safe_mailto('sro61@mail.ru','sro61@mail.ru');?> <?=anchor('http://sro61.ru/','http: sro61.ru');?></span><br/>
+						<span>Телефон: (863) 296-20-92, Факс: (863) 236-53-53</span><br/>
+						<span>E-mail: <?=safe_mailto('sro61@mail.ru','sro61@mail.ru');?>, http: <?=anchor('http://sro61.ru/','http: sro61.ru');?></span><br/>
 						<span>Рег. № СРО-Э-101</span><br/><br/>
 						<span>Исх. №_______ от __________________</span>
 						
 					</p>
 				</div>
-				<div class="span5">
+				<div class="span5 offset1">
 					<p class="center title">
 						<br/><br/><br/>
-						<span>Кому</span><br/>
-						<span><?=$passport['member'];?></span>
+						<strong>Кому</strong><br/>
+						<strong><?=$passport['member'];?></strong>
 						
 					</p>
 				</div>
 				<div class="clear"></div>
-				<br/><br/><br/>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span11">
+				<p>&nbsp;</p><p>&nbsp;</p>
 				<p class="center title">
-					<b>УВЕДОМЛЕНИЕ</b>
+					<strong>УВЕДОМЛЕНИЕ</strong>
 				</p>
 				<p class="intend">
-					Настоящим подтверждаем принятие и регистрацию энергетического паспорта, технического отчета, 
-					составленного по результатам обязательного энергетического обследования объекта: <?=$passport['customer'];?> 
+					Настоящим подтверждаем принятие и регистрацию энергетического паспорта,  
+					составленного по результатам энергетического обследования объекта: <?=$passport['customer'];?> 
 					(ИНН <?=$passport['inn'];?>), расположенного по адресу: <?=$passport['address'];?>, 
 					в соответствии с требованиями законодательства РФ, регистрационный № <?=$passport['number'];?>
 				</p>
+				<p>&nbsp;</p>
 				<p>
 					<table class="table no-border">
 						<tbody>
 							<tr>
-								<td width="75%">Директор<br/>СРО НП «Энергоаудит»</td>
-								<td width="25%">Л.К. Евкина</td>
-								<td>&nbsp;</td>
-								<td>&nbsp;</td>
+								<td width="85%">Директор<br/>СРО НП «Энергоаудит»</td>
+								<td width="15%"><em>Л.К. Евкина</em></td>
 							</tr>
 						</tbody>
 					</table>
@@ -55,5 +61,10 @@
 			</div>
 		</div>
 	</div>
+	<script type="text/javascript">
+		window.onload = function(){
+			//setTimeout(function() { window.print(); }, 1000);
+		};
+	</script>
 </body>
 </html>
